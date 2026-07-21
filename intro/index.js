b5c4dd98fc92e6196c0546fed292262783f88587
salhadin kedir <thevibe1122@gmail.com>
2026-07-21 14:15:16 +0300

    variables

---
+++ intro/index.js
@@ -0,0 +1,14 @@
+console.log(`hello my friend`)
+console.log(`i love pizza`)
+
+//window.alert(`this is a warning`)
+
+// this is a comment
+
+/* this
+is'
+a
+coomentg */
+
+document.getElementById('myheading').textContent = 'hello world';
+document.getElementById('myp').textContent = 'hello world';
---
+++ variables/index.js
@@ -0,0 +1,24 @@
+// let age = 23;
+// let price = 10.99;
+// let GPA = 3.7;
+// let online = true;
+// let fore_sale = false;
+// let first_name = "salah"
+// let last_name = "kedir"
+// let email = `salah@gmail.com`
+
+// console.log(`hello ${first_name} ${last_name}`);
+// console.log(`You are ${age} years old `);
+// console.log(`The price is ${price}`);
+// console.log(`Your GPA is ${GPA}`);
+// console.log(`Are you online? ${online}`);
+// console.log(`your email is ${email}`);
+// console.log(`Is the product for sale? ${fore_sale}`);
+
+let full_name = `Salhadin kedir`
+let age = 20
+let student = true
+
+document.getElementById("p1").textContent = `Hello ${full_name}`
+document.getElementById("p2").textContent = `You are ${age} years old`
+document.getElementById("p3").textContent = `Are you a student? ${student}`
