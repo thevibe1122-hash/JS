37c98eab7d399105fbc3789d33c1d6ddeeac0270
salhadin kedir <thevibe1122@gmail.com>
2026-07-21 14:27:29 +0300

    operations

---
+++ operations/index.js
@@ -0,0 +1,28 @@
+let student = 30
+
+// student = student + 1
+// student = student -1
+// student = student * 2
+// student = student / 2
+// student = student % 2
+
+student += 1 // student = student + 1
+student -= 1 // student = student - 1
+student *= 2 // student = student * 2
+student /= 2 // student = student / 2
+student %= 2 // student = student % 2
+
+let num = 10
+
+num-- // decrement
+num++ // increment
+--num // decrement (by one)
+++num // increment (by one)
+
+/* operator precedence 
+ 1.parenthesis ()
+ 2.exponents (**) 
+ 3.multiplication and division and modulous % 
+ 4.add and subtract
+
+*/
