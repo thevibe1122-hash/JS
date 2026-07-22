9fe4b970a5f83a2618deb4fff50333e198776fd2
salhadin kedir <thevibe1122@gmail.com>
2026-07-22 19:54:30 +0300

    data type

---
+++ type conversion/index.js
@@ -0,0 +1,19 @@
+// type conversion
+
+// let age = window.prompt(`how old are you?`);
+
+// age = Number(age)
+
+// age += 1
+
+// document.getElementById("myp").textContent = `your age next year will be ${age}`
+
+let a;
+let b;
+let c;
+
+x = Number(a)
+y = Boolean(b)
+z = String(c)
+
+console.log(x, y, z, typeof x, typeof y, typeof z);
