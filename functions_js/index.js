4d88ce154f4415235dacf0b54f96a4899fad94e3
salhadin kedir <thevibe1122@gmail.com>
2026-08-25 14:53:01 +0300

    modifications

--- functions_js/index.js
+++ functions_js/index.js
@@ -5,10 +5,18 @@ let b = `to you `
 let hb = h + b + `<hr>`
 let user = `Marshel`
 let age = ` 21 `
-
+let num = 2345678
 function hbd(user, age) {
     heading.innerHTML += hb + hb + h + `dear ${user},` + `<hr>` + `ur ${age} now,` + `<hr>` + hb
 }
 
-hbd(user, age);
-hbd(`barney`, `31`);        
+function isEven(num) {
+    return num % 2 === 0 ? true : false
+
+}
+
+function isValidEmail(email) {
+    return email.includes('@') && email.includes('.') ? true : false
+}
+
+heading.innerHTML = isValidEmail(`zbo@123.com`)
